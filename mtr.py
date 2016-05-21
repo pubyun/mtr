f77aba9251dd904d5c00fd3a46ed4db345bb81a8
Peng Yong <ppyy@pubyun.com>
2016-05-21 20:18:42 +0800

    bugfix：日志时间应该是发起进程的时候，而不是写日志的时间

--- mtr.py
+++ mtr.py
@@ -24,12 +24,13 @@ class Mtr(threading.Thread):
         self._cmd.append(self._ip)
 
     def run(self):
+        now = datetime.datetime.now()
         proc = subprocess.Popen(self._cmd, stdout=subprocess.PIPE)
         out, err = proc.communicate()
         fullname = os.path.join(LOGDIR, "%s.log" % self._ip)
         with open(fullname, "a") as f:
             if "Start: " not in out:
-                f.write("Start: %s\n" % unicode(datetime.datetime.now()))
+                f.write("Start: %s\n" % unicode(now))
             f.write(out)
 
 
